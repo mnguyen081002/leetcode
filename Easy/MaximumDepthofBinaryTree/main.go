c7497c0658d6222afcbf073dcc2f65a9fba9690d
Minh Nguyên <3langn@gmail.com>
2023-12-22 19:32:49 +0700

    Add MaximumDepthofBinaryTree implementation

---
+++ Easy/MaximumDepthofBinaryTree/main.go
@@ -0,0 +1,18 @@
+package main
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left := maxDepth(root.Left)
+	right := maxDepth(root.Right)
+	maxdep := max(left, right)
+	return 1 + maxdep
+}
