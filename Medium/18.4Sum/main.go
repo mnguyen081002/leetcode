e8e606ab6242b7368477f2117031e600b4a77fc5
Minh Nguyên <3langn@gmail.com>
2023-12-29 10:52:14 +0700

    Refactor kSum and twoSum functions

--- Medium/18.4Sum/main.go
+++ Medium/18.4Sum/main.go
@@ -7,8 +7,10 @@ func fourSum(nums []int, target int) [][]int {
 	return kSum(nums, 0, 4, target)
 }
 
+// key point = kSum(nums, i+1, k-1, target-nums[i])
 func kSum(nums []int, start, k, target int) [][]int {
 	res := [][]int{}
+	// không tìm thấy || 4 min của array lớn hơn target || 4 max của array nhỏ hơn target
 	if start == len(nums) || nums[start]*k > target || target > nums[len(nums)-1]*k {
 		return res
 	}
@@ -27,18 +29,23 @@ func kSum(nums []int, start, k, target int) [][]int {
 
 func twoSum(nums []int, start, target int) [][]int {
 	res := [][]int{}
-	lo, hi := start, len(nums)-1
-	for lo < hi {
-		sum := nums[lo] + nums[hi]
-		if sum < target || (lo > start && nums[lo] == nums[lo-1]) {
-			lo++
-		} else if sum > target || (hi < len(nums)-1 && nums[hi] == nums[hi+1]) {
-			hi--
+	low, high := start, len(nums)-1
+	for low < high {
+		sum := nums[low] + nums[high]
+		if sum < target || (low > start && nums[low] == nums[low-1]) {
+			low++
+		} else if sum > target || (high < len(nums)-1 && nums[high] == nums[high+1]) {
+			high--
 		} else {
-			res = append(res, []int{nums[lo], nums[hi]})
-			lo++
-			hi--
+			res = append(res, []int{nums[low], nums[high]})
+			low++
+			high--
 		}
 	}
 	return res
+}
+
+func main() {
+	nums := []int{1, 0, -1, 0, -2, 2}
+	fourSum(nums, 0)
 }
