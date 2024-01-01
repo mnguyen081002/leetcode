edfd45b98db74f8f31af865f6d9f7eec3c1a3533
Minh Nguyên <3langn@gmail.com>
2024-01-01 20:52:51 +0700

    Refactor fourSum function and add missing import statement

--- Medium/18.4Sum/main.go
+++ Medium/18.4Sum/main.go
@@ -1,51 +1,55 @@
 package main
 
-import "sort"
+import (
+	"fmt"
+	"slices"
+)
 
-func fourSum(nums []int, target int) [][]int {
-	sort.Ints(nums)
-	return kSum(nums, 0, 4, target)
+func twoSum(nums []int, start, target int) [][]int {
+	sum := 0
+	result := [][]int{}
+	end := len(nums) - 1
+	for start < end {
+		sum = nums[start] + nums[end]
+		if sum == target {
+			result = append(result, []int{nums[start], nums[end]})
+			start++
+			end--
+		} else if sum < target {
+			start++
+		} else {
+			end--
+		}
+	}
+
+	return result
 }
 
-// key point = kSum(nums, i+1, k-1, target-nums[i])
-func kSum(nums []int, start, k, target int) [][]int {
-	res := [][]int{}
-	// không tìm thấy || 4 min của array lớn hơn target || 4 max của array nhỏ hơn target
-	if start == len(nums) || nums[start]*k > target || target > nums[len(nums)-1]*k {
-		return res
+func kSum(nums []int, k, start, target int) [][]int {
+	result := [][]int{}
+	if start > len(nums)-1 || k < 2 || nums[start]*k > target || nums[len(nums)-1]*k < target {
+		return [][]int{}
 	}
+
 	if k == 2 {
 		return twoSum(nums, start, target)
 	}
+
 	for i := start; i < len(nums); i++ {
-		if i == start || nums[i-1] != nums[i] {
-			for _, set := range kSum(nums, i+1, k-1, target-nums[i]) {
-				res = append(res, append([]int{nums[i]}, set...))
-			}
+		for _, res := range kSum(nums, k-1, i+1, target-nums[i]) {
+			result = append(result, append([]int{nums[i]}, res...))
 		}
 	}
-	return res
+
+	return result
 }
 
-func twoSum(nums []int, start, target int) [][]int {
-	res := [][]int{}
-	low, high := start, len(nums)-1
-	for low < high {
-		sum := nums[low] + nums[high]
-		if sum < target || (low > start && nums[low] == nums[low-1]) {
-			low++
-		} else if sum > target || (high < len(nums)-1 && nums[high] == nums[high+1]) {
-			high--
-		} else {
-			res = append(res, []int{nums[low], nums[high]})
-			low++
-			high--
-		}
-	}
-	return res
+func fourSum(nums []int, target int) [][]int {
+	slices.Sort(nums)
+	return kSum(nums, 4, 0, target)
 }
 
 func main() {
 	nums := []int{1, 0, -1, 0, -2, 2}
-	fourSum(nums, 0)
+	fmt.Println(fourSum(nums, 0))
 }
--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
 - 207.CourseSchedule
 - 76.MinimumWindowSubstring
-- 18.4Sum
 - 455.AssignCookies
