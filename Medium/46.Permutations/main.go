fdec59a4d1b3b07e7f52817cefea00fe756e003b
Minh Nguyên <3langn@gmail.com>
2024-01-17 13:23:18 +0700

    Add permutation function to generate all possible permutations of a given slice

--- Medium/46.Permutations/main.go
+++ Medium/46.Permutations/main.go
@@ -1,1 +1,37 @@
 package main
+
+import "slices"
+
+func permute(nums []int) [][]int {
+	res := [][]int{}
+	backtracking(nums, []int{}, &res)
+	return res
+}
+
+func backtracking(nums []int, cur []int, res *[][]int) {
+	if len(cur) == len(nums) {
+		nw := make([]int, len(cur))
+		copy(nw, cur)
+		*res = append(*res, nw)
+	} else {
+		for i := 0; i < len(nums); i++ {
+			if slices.Contains(cur, nums[i]) {
+				continue
+			}
+			cur = append(cur, nums[i])
+			backtracking(nums, cur, res)
+			cur = cur[:len(cur)-1]
+		}
+	}
+}
+
+func main() {
+	r := permute([]int{1, 2, 3})
+
+	for _, v := range r {
+		for _, vv := range v {
+			print(vv, " ")
+		}
+		println()
+	}
+}
