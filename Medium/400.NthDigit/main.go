aec1d0e75931c43db87e8dba1d77be497dbbe05e
Minh Nguyên <3langn@gmail.com>
2024-01-28 10:05:25 +0700

    Refactor findNthDigit function to improve readability and performance

--- Medium/400.NthDigit/main.go
+++ Medium/400.NthDigit/main.go
@@ -1,20 +1,38 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 // TODO: handle this
 func findNthDigit(n int) int {
-	if n < 10 {
-		return n
+	digit, start, count := 1, 1, 9
+	for n > count {
+		n -= count
+		digit += 1
+		start *= 10
+		count = 9 * start * digit
 	}
-	x, i, num := 0, 1, 1
-	for i <= n {
-		x = num / 10
-		for x != 0 && i <= n {
-			x = num / 10
-			i++
-		}
-		i++
-		num++
-	}
+	num := start + (n-1)/digit
+	return int(strconv.Itoa(num)[(n-1)%digit] - '0')
+}
 
-	return x
+// func findNthDigit(n int) int {
+//     if n < 10 {
+//         return n
+//     }
+
+//     if n % 2 == 0 {
+//         return (n - 10) / 20 + 1
+//     }else {
+//         if n / 10 % 2 == 0 {
+//             return (n % 10 + 10) / 2
+//         }
+//         return n % 10 / 2
+//     }
+// }
+
+func main() {
+	fmt.Println(1 / 10)
 }
