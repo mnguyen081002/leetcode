0f69953f51bea87529b2b1ae06fc4e75608b80af
Minh Nguyên <3langn@gmail.com>
2024-02-05 18:00:18 +0700

    Implement maxPathSum function for binary tree

--- Hard/124. Binary Tree Maximum Path Sum/main.go
+++ Hard/124. Binary Tree Maximum Path Sum/main.go
@@ -1,4 +1,29 @@
 package main
 
+import "math"
+
 // testcase: [-9,9,20,11,16,15,7]
 // expected: 51
+// [-9,9,20,11,16,15,7,null,null,10,5,null,null,null,null]
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func maxPathSum(root *TreeNode) int {
+	mx := root.Val
+	dfs(root, &mx)
+	return mx
+}
+
+func dfs(root *TreeNode, mx *int) int {
+	if root == nil {
+		return math.MinInt
+	}
+	maxSL := max(dfs(root.Left, mx), 0)
+	maxSR := max(dfs(root.Right, mx), 0)
+	*mx = max(*mx, root.Val+maxSL+maxSR)
+	return root.Val + max(maxSL, maxSR)
+}
