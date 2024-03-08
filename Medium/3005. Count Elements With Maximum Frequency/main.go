e840ef0e3b6c8b67700c36849ea2d38b2e660b2f
Minh Nguyên <3langn@gmail.com>
2024-03-08 11:06:03 +0700

    Refactor maxFrequencyElements function

--- Medium/3005. Count Elements With Maximum Frequency/main.go
+++ Medium/3005. Count Elements With Maximum Frequency/main.go
@@ -1,18 +1,18 @@
 package main
 
 func maxFrequencyElements(nums []int) int {
-    countMax, countSame,m := 0,0, map[int]int{}
+	countMax, countSame, m := 0, 0, map[int]int{}
 
-    for _, v := range nums {
-        m[v]++
-        if m[v] > countMax {
-            countMax = m[v]
-            countSame = 0
-        }
-        if m[v] == countMax {
-            countSame++
-        }
-    }
+	for _, v := range nums {
+		m[v]++
+		if m[v] > countMax {
+			countMax = m[v]
+			countSame = 0
+		}
+		if m[v] == countMax {
+			countSame++
+		}
+	}
 
-    return countMax*countSame
+	return countMax * countSame
 }
